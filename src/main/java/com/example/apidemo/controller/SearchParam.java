13734db2808ac30a33910b7d2ea904c0ac6829d1
haerimRoh <mtxpvksh@gmail.com>
2023-10-26 17:13:09 +0900

    feat. SearchParam에 Schema추가, swagger UI 적용

--- src/main/java/com/example/apidemo/controller/SearchParam.java
+++ src/main/java/com/example/apidemo/controller/SearchParam.java
@@ -1,12 +1,16 @@
 package com.example.apidemo.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class SearchParam {
+    @Schema(description = "사용자 계정이름",example = "hellobuilder1")
     private String account;
+    @Schema(description = "사용자 이메일",example = "abc@pipebuilders.com")
     private String email;
+    @Schema(description = "요청할 페이지",defaultValue = "1",example = "30")
     private int page;
 }
