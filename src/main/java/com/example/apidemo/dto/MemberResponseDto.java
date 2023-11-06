74253e70d1f939e302c7c55a04c34a399d87adf4
haerimRoh <mtxpvksh@gmail.com>
2023-11-06 23:06:51 +0900

    feat. MemberResponseDto swagger UI Schema 설명추가

--- src/main/java/com/example/apidemo/dto/MemberResponseDto.java
+++ src/main/java/com/example/apidemo/dto/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.apidemo.dto;
 
 import com.example.apidemo.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "회원가입 응답 DTO")
 public class MemberResponseDto {
+    @Schema(description = "이메일",example = "love@pipebuilder.com")
     private String email;
 
     public static MemberResponseDto of(Member member) {
